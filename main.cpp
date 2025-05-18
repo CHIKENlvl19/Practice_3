e00aa3ace921b2d07c2a025e86bf4536f5d49b1f
bloodfallen <74541116+CHIKENlvl19@users.noreply.github.com>
2025-05-19 01:39:22 +0700

    main func done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 int main() {
 
+    cout << "Введите номер задачи (от 1 до 10): ";
+    
+
+    while (true)
+    {
+        int choice;
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                return 0;
+
+        
+            default:
+                cout << "Неверный номер задачи, пропробуйте еще раз." << endl << "Номер задачи: ";
+        }
+
+    }
+
+    
     return 0;
 }
